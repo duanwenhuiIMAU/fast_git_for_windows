265965698f9acf7936ef7f8ca41a9f99d293b095
duanwenhuiIMAU <2254422301@qq.com>
2020-08-02 18:45:48 +0800

    init

---
+++ tree.ps1
@@ -0,0 +1,43 @@
+tree . /f | Out-File -Encoding default tree.txt
+if (Test-Path .git){
+    git diff tree.txt | Out-File -Encoding default diff.txt 
+    $plusLine = Get-content diff.txt|select-string "\+"
+    $plusLine = $plusLine -notlike "*@@*"
+    $plusLine = $plusLine -notlike "*+++*"
+    $minsLine = Get-content diff.txt|select-string "\-"
+    $minsLine = $minsLine -notlike "*@@*"
+    $minsLine = $minsLine -notlike "*---*"
+    $minsLine = $minsLine -notlike "*diff --git*"
+    $str = ""
+    if ($plusLine.count -ne 0){
+        for ($i=0;$i -le $plusLine.count;$i++){
+                $str += $plusLine[$i]
+            # if($i -gt 5){
+            #     break
+            # }
+        }
+            $str += "    =>加   "
+    }
+    if ($minsLine.count -ne 0){
+        for ($i=0;$i -le $minsLine.count;$i++){
+                $str += $minsLine[$i]
+            #  if($i -gt 5){
+            #     break
+            # }
+        }
+            $str += "    =>减   "
+    }
+    Remove-Item diff.txt
+    $str += "|" + $( Get-Date -Format 'yyyy/M/d/H/m' )
+    git add tree.txt
+    git ci -m $str
+}
+else {
+    git init 
+    echo "" > tree.txt
+    echo "" > README.md
+    echo "" > .gitignore
+    echo "" > CodeAndGit.bat
+    git add tree.txt
+    git ci -m  "文件状态初始化"
+}
